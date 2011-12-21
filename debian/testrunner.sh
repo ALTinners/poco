67640fd58d0009b90d9a80cc89a8c16fc047b619
Cristian Greco <cristian@debian.org>
2011-12-21 16:03:17 +0100

    debian/rules: enable selected tests using a testrunner.sh script.

---
+++ debian/testrunner.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Author: Cristian Greco <cristian@debian.org>
+
+ENABLED_TEST_LIBS="Crypto Data Data/SQLite Foundation Net Util XML"
+DISABLED_TEST_LIBS="Data/MySQL Data/ODBC NetSSL_OpenSSL Zip"
+
+TESTDIR=$(mktemp -d /tmp/pocotest.XXXXXX)
+cp -d lib/*/*/* ${TESTDIR}
+
+for testlib in ${ENABLED_TEST_LIBS}; do
+  TESTRUNNER=$(find ${testlib}/testsuite -type f -name testrunner)
+  cp ${TESTRUNNER} ${TESTDIR}
+  (
+    cd ${TESTDIR}; \
+    echo "[Running test: ${testlib}]"; \
+    export LD_LIBRARY_PATH=.:${LD_LIBRARY_PATH}; \
+    ./testrunner -all; \
+    echo; \
+  )
+done
+
+rm -rf ${TESTDIR}
