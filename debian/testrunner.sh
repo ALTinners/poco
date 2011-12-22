315f19e325301ef040a7d66a990f7c6fd8105f8f
Cristian Greco <cristian@debian.org>
2011-12-22 22:12:29 +0100

    debian/testrunner.sh: coy some more files needed by the Foundation testsuite.

--- debian/testrunner.sh
+++ debian/testrunner.sh
@@ -5,11 +5,17 @@
 ENABLED_TEST_LIBS="Crypto Data Data/SQLite Foundation Net Util XML"
 DISABLED_TEST_LIBS="Data/MySQL Data/ODBC NetSSL_OpenSSL Zip"
 
+# copy libraries to temp dir
 TESTDIR=$(mktemp -d /tmp/pocotest.XXXXXX)
 LIBSDIR=$(find lib/ -name libPocoFoundation.so -printf '%h')
-
 cp -d ${LIBSDIR}/* ${TESTDIR}
 
+# additional files needed for the Foundation testsuite
+FILE_TESTAPP=$(find Foundation/testsuite/ -type f -name TestApp)
+FILE_TESTLIB=$(find Foundation/testsuite/ -type f -name TestLibrary.so)
+cp ${FILE_TESTAPP} ${FILE_TESTLIB} ${TESTDIR}
+
+# run tests
 for testlib in ${ENABLED_TEST_LIBS}; do
   TESTRUNNER=$(find ${testlib}/testsuite -type f -name testrunner)
   cp ${TESTRUNNER} ${TESTDIR}
